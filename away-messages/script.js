485a90460df0d95c99d49b7b1f6e5ac6cd9fedf9
Julia Kim <minhee68@yahoo.com>
2019-12-11 18:19:20 -0500

    Add insert link function

--- away-messages/script.js
+++ away-messages/script.js
@@ -11,6 +11,12 @@ document.addEventListener('click', function(e) {
   }
 });
 
+function getSmiley(img) {
+  var smiley = img.src;
+  document.getElementById('text-field').innerHTML +=
+    '<img src="' + smiley + '">';
+}
+
 document
   .getElementsByClassName('text-color')[0]
   .addEventListener('click', function() {
@@ -29,25 +35,27 @@ document
 
 function getColor() {
   var color = document.getElementById('colorPicker').value;
-  document.getElementById('colorful').style.color = color;
+  document.getElementById('text-field').style.color = color;
 }
 
 function getBackgroundColor() {
   var color = document.getElementById('bgColorPicker').value;
-  document.getElementById('colorful').style.backgroundColor = color;
+  document.getElementById('text-field').style.backgroundColor = color;
 }
 
-function getSmiley(img) {
-  var smiley = img.src;
-  document.getElementsByClassName('text-field')[0].innerHTML +=
-    '<img src="' + smiley + '">';
+var minSize = 0.5;
+var maxSize = 5;
+
+function resizeText(multiplier) {
+  var elem = document.getElementById('text-field');
+  var currentSize = elem.style.fontSize || 1;
+  var newSize = parseFloat(currentSize) + multiplier * 0.2;
+  if (newSize < minSize) newSize = minSize;
+  if (newSize > maxSize) newSize = maxSize;
+  elem.style.fontSize = newSize + 'em';
 }
 
-function link() {
-  var linkURL = prompt('Enter URL:', 'http://');
-  document.execCommand('CreateLink', false, linkURL);
-}
-
+// https://stackoverflow.com/questions/5605401/insert-link-in-contenteditable-element
 function saveSelection() {
   if (window.getSelection) {
     sel = window.getSelection();
@@ -75,5 +83,68 @@ function restoreSelection(savedSel) {
     } else if (document.selection && savedSel.select) {
       savedSel.select();
     }
+  }
+}
+
+function getLinksInSelection() {
+  var selectedLinks = [];
+  var range, containerEl, links, linkRange;
+  if (window.getSelection) {
+    sel = window.getSelection();
+    if (sel.getRangeAt && sel.rangeCount) {
+      linkRange = document.createRange();
+      for (var r = 0; r < sel.rangeCount; ++r) {
+        range = sel.getRangeAt(r);
+        containerEl = range.commonAncestorContainer;
+        if (containerEl.nodeType != 1) {
+          containerEl = containerEl.parentNode;
+        }
+        if (containerEl.nodeName.toLowerCase() == 'a') {
+          selectedLinks.push(containerEl);
+        } else {
+          links = containerEl.getElementsByTagName('a');
+          for (var i = 0; i < links.length; ++i) {
+            linkRange.selectNodeContents(links[i]);
+            if (
+              linkRange.compareBoundaryPoints(range.END_TO_START, range) < 1 &&
+              linkRange.compareBoundaryPoints(range.START_TO_END, range) > -1
+            ) {
+              selectedLinks.push(links[i]);
+            }
+          }
+        }
+      }
+      linkRange.detach();
+    }
+  } else if (document.selection && document.selection.type != 'Control') {
+    range = document.selection.createRange();
+    containerEl = range.parentElement();
+    if (containerEl.nodeName.toLowerCase() == 'a') {
+      selectedLinks.push(containerEl);
+    } else {
+      links = containerEl.getElementsByTagName('a');
+      linkRange = document.body.createTextRange();
+      for (var i = 0; i < links.length; ++i) {
+        linkRange.moveToElementText(links[i]);
+        if (
+          linkRange.compareEndPoints('StartToEnd', range) > -1 &&
+          linkRange.compareEndPoints('EndToStart', range) < 1
+        ) {
+          selectedLinks.push(links[i]);
+        }
+      }
+    }
+  }
+  return selectedLinks;
+}
+
+function link(type) {
+  var savedSel = saveSelection();
+  var url = prompt('Enter URL:', 'http://');
+  restoreSelection(savedSel);
+  document.execCommand('CreateLink', false, url);
+  var links = getLinksInSelection();
+  for (var i = 0; i < links.length; ++i) {
+    links[i].setAttribute('target', '_blank');
   }
 }
