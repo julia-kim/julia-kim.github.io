1faffee753258fb283418cb237c67f1d9000c462
Julia Kim <minhee68@yahoo.com>
2020-07-01 18:01:26 -0400

    Update portfolio

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ toggle.addEventListener('change', function() {
 document.getElementById('defaultOpen').click();
 
 function openProject(evt, project) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName('tabcontent');
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none';
+  var i, content, links;
+  content = document.getElementsByClassName('tab-content');
+  for (i = 0; i < content.length; i++) {
+    content[i].style.display = 'none';
   }
-  tablinks = document.getElementsByClassName('tablinks');
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  links = document.getElementsByClassName('tab-link');
+  for (i = 0; i < links.length; i++) {
+    links[i].className = links[i].className.replace(' active', '');
   }
   document.getElementById(project).style.display = 'block';
   evt.currentTarget.className += ' active';
